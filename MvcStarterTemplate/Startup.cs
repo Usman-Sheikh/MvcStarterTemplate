9f77e3d34a7cbc8058197931b622d09bf3f76e4a
Usman-Sheikh <sh.usmanikram@hotmail.com>
2016-03-24 21:28:47 +0500

    Initial Commit

---
+++ MvcStarterTemplate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcStarterTemplate.Startup))]
+namespace MvcStarterTemplate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
